1098f789e40af0d97f0869ac5dd6a47ab0a5ff31
aaronlee <paperscn@126.com>
2025-01-10 08:55:56 +0800

    Initial commit

---
+++ piano-app/script.js
@@ -0,0 +1,62 @@
+const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+
+function playNote(frequency) {
+    const oscillator = audioContext.createOscillator();
+    const gainNode = audioContext.createGain();
+    
+    oscillator.frequency.value = frequency;
+    oscillator.type = 'sine';
+    
+    gainNode.gain.setValueAtTime(0, audioContext.currentTime);
+    gainNode.gain.linearRampToValueAtTime(1, audioContext.currentTime + 0.01);
+    gainNode.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + 1);
+    
+    oscillator.connect(gainNode);
+    gainNode.connect(audioContext.destination);
+    
+    oscillator.start();
+    oscillator.stop(audioContext.currentTime + 1);
+}
+
+const noteFrequencies = {
+    'C4': 261.63,
+    'C#4': 277.18,
+    'D4': 293.66,
+    'D#4': 311.13,
+    'E4': 329.63,
+    'F4': 349.23,
+    'F#4': 369.99,
+    'G4': 392.00,
+    'G#4': 415.30,
+    'A4': 440.00,
+    'A#4': 466.16,
+    'B4': 493.88,
+    'C5': 523.25,
+    'C#5': 554.37,
+    'D5': 587.33,
+    'D#5': 622.25,
+    'E5': 659.25,
+    'F5': 698.46,
+    'F#5': 739.99,
+    'G5': 783.99,
+    'G#5': 830.61,
+    'A5': 880.00,
+    'A#5': 932.33,
+    'B5': 987.77
+};
+
+document.querySelectorAll('.key').forEach(key => {
+    key.addEventListener('mousedown', () => {
+        const note = key.dataset.note;
+        playNote(noteFrequencies[note]);
+        key.classList.add('active');
+    });
+    
+    key.addEventListener('mouseup', () => {
+        key.classList.remove('active');
+    });
+    
+    key.addEventListener('mouseleave', () => {
+        key.classList.remove('active');
+    });
+});
